65317ac288d1a8c3838744d3f5d3131f34da97f7
PRANESHWAR G <144886491+praneshwar05@users.noreply.github.com>
2023-10-06 13:53:19 +0530

    Delete Day 11/src directory

--- Day 11/src/main/java/com/example/demo/proj2/exception/PatientNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.proj2.exception;
-
-public class PatientNotFoundException extends RuntimeException {
-	  
-	public PatientNotFoundException(int patient_id){
-	        super("Could not found the patient with id "+ patient_id);
-	    }
-}
-
