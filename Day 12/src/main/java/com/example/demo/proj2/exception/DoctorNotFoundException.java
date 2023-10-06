5267a1027fcff3ec2fbe855aaa8e07ff21738fb0
PRANESHWAR G <144886491+praneshwar05@users.noreply.github.com>
2023-10-06 13:53:31 +0530

    Delete Day 12/src directory

--- Day 12/src/main/java/com/example/demo/proj2/exception/DoctorNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.proj2.exception;
-
-public class DoctorNotFoundException extends RuntimeException{
-	public DoctorNotFoundException(int id){
-        super("Could not found the doctor with id "+ id);
-    }
-
-}
--- Day 12/src/main/java/com/example/demo/proj2/service/SignupServiceInt.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.proj2.service;
-
-public interface SignupServiceInt {
-
-}
