3d8afa65e6582ed30897504d6b9871c243b7dc2e
PRANESHWAR G <144886491+praneshwar05@users.noreply.github.com>
2023-10-06 14:19:51 +0530

    Add files via upload

---
+++ Day 15/src/main/java/com/example/demo/proj2/repo/LoginRepoInt.java
@@ -0,0 +1,20 @@
+package com.example.demo.proj2.repo;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.proj2.Entity.LoginEntity;
+
+@Repository
+
+public interface LoginRepoInt extends JpaRepository<LoginEntity,Integer>{
+
+	LoginEntity findByEmail(String email);
+	
+
+}
+
+
+	
---
+++ Day 15/src/main/java/com/example/demo/proj2/service/ForgotPasswordServiceInt.java
@@ -0,0 +1,12 @@
+package com.example.demo.proj2.service;
+
+import java.util.List;
+
+import com.example.demo.proj2.Entity.ForgotPasswordEntity;
+
+public interface ForgotPasswordServiceInt {
+
+	public List<ForgotPasswordEntity> getUsers();
+	
+
+}
