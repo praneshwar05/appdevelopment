429530520c45478ae75fe79b7d4649aee4aa382d
PRANESHWAR G <144886491+praneshwar05@users.noreply.github.com>
2023-10-09 12:03:05 +0530

    Add files via upload

---
+++ day 6/src/components/header/headerComponents/HeaderLogoDiv.jsx
@@ -0,0 +1,14 @@
+import HeaderSearchBox from "./HeaderSearchBox";
+//import LOGO from "../../../asserts/logo.png";
+import "./style/headerlogodiv.css";
+
+export default function HeaderLogoDiv() {
+  return (
+    <>
+      <div className="headerlogodiv-wrapper">
+       
+        <HeaderSearchBox />
+      </div>
+    </>
+  );
+}
