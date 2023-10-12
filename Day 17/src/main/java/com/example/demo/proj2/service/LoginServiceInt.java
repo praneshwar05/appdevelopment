7cbddd9b3bd9dbdf30c33e8114772f7d68e441da
PRANESHWAR G <144886491+praneshwar05@users.noreply.github.com>
2023-10-12 15:18:13 +0530

    Delete Day 17 directory

--- Day 17/src/main/java/com/example/demo/proj2/service/LoginServiceInt.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.proj2.service;
-
-public interface LoginServiceInt {
-
-}
-
-
--- Day 17/src/main/java/com/example/demo/proj2/service/SignupService.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.proj2.service;
-
-public class SignupService implements SignupServiceInt {
-
-}
